f23b3271aba60f62da169f5ee69b3f872bb55d2d
Medric Sonwa <medric49@gmail.com>
2021-09-14 22:34:46 -0400

    qsdqsd

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Author : Medric Sonwa
 
 from sys import stdin
+import os
+
 
 if __name__ == '__main__':
-    print(stdin.read())
+    target_dir = stdin.read().split('\n')[0]
+    files = os.listdir(target_dir)
+    files = sorted(files)
+
+    print(files)
+
